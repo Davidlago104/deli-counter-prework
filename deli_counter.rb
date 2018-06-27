4fd83951830c705093edd6f87e2072d1892f560c
Davidlago104 <davidlago104@gmail.com>
2018-06-27 16:45:17 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,6 +13,9 @@ def line(array)
     puts "The line is currently empty."
   else 
     message = "The line is currently:"
-  array.each_index do |value, index|
-    message += "#{index.to"
+  array.each_with_index do |name, index|
+    message += "#{index + 1 }. #{name}"
+  end
+  puts message
+end
 end
