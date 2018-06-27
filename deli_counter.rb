38add2b0604763d964ff33762c910604a40c7e0f
Davidlago104 <davidlago104@gmail.com>
2018-06-27 16:35:36 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+katz_deli = []
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  line = katz_deli.index(name)
+  puts "Welcome, #{string}. You are number #{other_deli.index(name)+1} in line."
+  return name, line
+end
